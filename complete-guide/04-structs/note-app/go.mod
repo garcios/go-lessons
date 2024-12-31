32b31d9de47816c4c20524f1731557117b516b07
oskiegarcia <ogarc74@gmail.com>
2024-12-31 20:00:44 +1100

    add note app

---
+++ complete-guide/04-structs/note-app/go.mod
@@ -0,0 +1,3 @@
+module github.com/oskiegarcia/note-app
+
+go 1.22.2
---
+++ complete-guide/04-structs/note-app/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/oskiegarcia/note-app/note"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	title, content := getNoteData()
+
+	userNote, err := note.New(title, content)
+	if err != nil {
+		fmt.Println("Error creating note:", err)
+		return
+	}
+
+	userNote.Display()
+	err = userNote.Save()
+	if err != nil {
+		fmt.Println("Error saving note:", err)
+		return
+	}
+
+	fmt.Println("Saving note succeeded!")
+
+}
+
+func getNoteData() (string, string) {
+	title := getUserInput("Note title: ")
+	content := getUserInput("Note content: ")
+
+	return title, content
+}
+
+func getUserInput(prompt string) string {
+	fmt.Print(prompt)
+	var value string
+
+	reader := bufio.NewReader(os.Stdin)
+	value, err := reader.ReadString('\n')
+	if err != nil {
+		return ""
+	}
+
+	value = strings.TrimSuffix(value, "\n")
+	value = strings.TrimSuffix(value, "\r")
+
+	return value
+}
