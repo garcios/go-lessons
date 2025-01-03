14bc345a988ccfb5ad1eea2d226142d1a66bb1df
oskiegarcia <ogarc74@gmail.com>
2025-01-03 15:27:19 +1100

    modify variadic functions

--- complete-guide/07-functions/variadic.go
+++ complete-guide/07-functions/variadic.go
@@ -8,12 +8,15 @@ func variadic() {
 	fmt.Println(sum(1, 2, 3, 4, 5)) //prints 15
 
 	nums := []int{1, 2, 3, 4, 5}
+
+	// splitting slice into parameter values using ... operator
 	fmt.Println(sum(nums...)) //prints 15
 
 	fmt.Println("Hello", "Oscar", "How", "are", "you?") //prints "Hello Oscar How are you?"
 
 }
 
+// nums is a slice
 func sum(nums ...int) int {
 	total := 0
 	for _, num := range nums {
