1100e9047b76ba694a2853a8d45b8b3bdd6181c9
oskiegarcia <ogarc74@gmail.com>
2025-01-04 12:16:09 +1100

    add some comments

--- complete-guide/08-proj-calculator/main.go
+++ complete-guide/08-proj-calculator/main.go
@@ -3,16 +3,21 @@ package main
 import (
 	"fmt"
 	filem "github.com/oskiegarcia/price-calculator/filemanager"
+	io "github.com/oskiegarcia/price-calculator/iomanager"
 	p "github.com/oskiegarcia/price-calculator/prices"
 )
 
 func main() {
 	taxRates := []float64{0, 0.07, 0.10, 0.15}
 
+	var iom io.IOManager
 	for _, taxRate := range taxRates {
-		fm := filem.New("prices.txt", fmt.Sprintf("rseult_%.0f.json", taxRate*100))
-		//cmd := cmdmanager.New()
-		priceJob := p.NewTaxIncludedPriceJob(fm, taxRate)
+		iom = filem.New("prices.txt", fmt.Sprintf("rseult_%.0f.json", taxRate*100))
+
+		// Note: iom could be replaced my CMDManager to take inputs from command line and outputs to console.
+		//iom = cmdmanager.New()
+
+		priceJob := p.NewTaxIncludedPriceJob(iom, taxRate)
 		err := priceJob.Process()
 		if err != nil {
 			fmt.Printf("Error processing price job: %v\n", err)
